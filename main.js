46aee441694de762e9c0e0991ba08116ad81e19b
Soraya Rezaie <banorezaie2004@gmail.com>
2024-12-24 11:37:46 +0430

    backtick used

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ console.log(typeof y);
 let t = " ";
 console.log(typeof t);
 
-let fname = "Ahmad";
+//let fname = "Ahmad";
+//let lname = "Rezaie";
+//console.log(fname , lname);
+
+
+//let name = "Soraya";
+//let pname = "Engener";
+//console.log("my name is : " + name + " " + "and my position is : "+ pname);
+
+
+let fname = "Soraya";
 let lname = "Rezaie";
-console.log(fname , lname);
+console.log( `my firstname is ${fname} and my lastname is ${lname}`);
 
-
-let name = "Soraya";
-let pname = "Engener";
-console.log("my name is : " + name + " " + "and my position is : "+ pname);
