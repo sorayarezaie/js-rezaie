7851e41ba4b97d2075a08e9dd52a5c956041a51b
Soraya Rezaie <banorezaie2004@gmail.com>
2024-12-24 11:23:46 +0430

    concatenate added

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ console.log(typeof y);
 
 let t = " ";
 console.log(typeof t);
+
+let fname = "Ahmad";
+let lname = "Rezaie";
+console.log(fname , lname);
+
+
+let name = "Soraya";
+let pname = "Engener";
+console.log("my name is : " + name + " " + "and my position is : "+ pname);
