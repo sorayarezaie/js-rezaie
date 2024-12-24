a8ae0b0ced42608d03d49e864f87ed84150bdc3d
Soraya Rezaie <banorezaie2004@gmail.com>
2024-12-24 12:01:34 +0430

    number added

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let x = "Ali";
-console.log(typeof x);
+//let x = "Ali";
+//console.log(typeof x);
 
-let y = "1234758";
-console.log(typeof y);
+//let y = "1234758";
+//console.log(typeof y);
 
-let t = " ";
-console.log(typeof t);
+//let t = " ";
+//console.log(typeof t);
 
 //let fname = "Ahmad";
 //let lname = "Rezaie";
@@ -22,6 +22,16 @@ console.log(typeof t);
 //console.log( `my firstname is ${fname} and my lastname is ${lname}`);
 
 
-let phnumber = "0795149503";
-let hnumber = "0794842517";
-console.log( `my phon number is ${phnumber} and my home number is ${hnumber}`);
+//let phnumber = "0795149503";
+//let hnumber = "0794842517";
+//console.log( `my phon number is ${phnumber} and my home number is ${hnumber}`);
+
+
+let x = 2334;
+console.log(typeof x);
+
+let y = 34.7984;
+console.log(typeof y);
+
+let r = 57453848587480393902874n;
+console.log(typeof r);
